94131bada603c09ef261c5e5cc232156f835a9f6
Yonas Baysasaw Kibret <yonas.baysasaw.kibret@gmail.com>
2025-10-23 14:14:19 +0300

    Add HelloWorld.java to print 'Hello, World!'

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+// This is a simple Java program to print "Hello, World!"
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Prints the message to the console
+    }
+}
